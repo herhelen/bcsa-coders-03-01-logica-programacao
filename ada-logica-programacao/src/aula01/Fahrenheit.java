3ef48a91210bd3a66115053d5aa380580d9e91f1
Helen Her <her.helen@gmail.com>
2023-07-12 22:33:51 -0300

    adicionando arquivos da aula 1 e atividade 1

---
+++ ada-logica-programacao/src/aula01/Fahrenheit.java
@@ -0,0 +1,31 @@
+package aula01;
+
+import java.util.Scanner;
+
+public class Fahrenheit {
+
+    /*
+    * Atividade 1
+    * Faça um Programa que peça a temperatura em graus Fahrenheit,
+    * transforme e mostre a temperatura em graus Celsius.
+    * C = 5 * ((F-32) / 9).
+    */
+
+    public static double fahrenheitToCelsius(double fahrenheit) {
+        return 5 * ((fahrenheit -32) / 9);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite a temperatura em graus Fahrenheit:");
+
+        double fahrenheit = sc.nextDouble();
+        double celsius = fahrenheitToCelsius(fahrenheit);
+
+        System.out.printf("O graus Fahrenheit %.2f em graus Celsius é %.2f.", fahrenheit, celsius);
+
+        sc.close();
+    }
+
+}
---
+++ ada-logica-programacao/src/aula01/Hello.java
@@ -0,0 +1,11 @@
+package aula01;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println(mensagemBoasVindas());
+    }
+
+    public static String mensagemBoasVindas() {
+        return "aula01.Hello World!";
+    }
+}
