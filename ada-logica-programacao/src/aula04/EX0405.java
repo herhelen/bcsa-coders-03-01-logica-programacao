e0701a252e5763bda79050cf301bb74e8c6cf7d7
Helen Her <her.helen@gmail.com>
2023-07-19 20:50:23 -0300

    adicionando resolução para exercício 5 da aula 4 (é igual ao exercício 4 da aula 3)

---
+++ ada-logica-programacao/src/aula04/EX0405.java
@@ -0,0 +1,64 @@
+package aula04;
+
+public class EX0405 {
+    public static void main(String[] args) {
+        String[] entradas = {
+                "amor",
+                "cocada",
+                "mora",
+                "ovo",
+                "cachorro",
+                "kaka",
+                "saudades",
+                "palalelepipedo",
+                "fofocalizando",
+                "papagaio",
+                "ateiaateiab"
+        };
+
+        int[] saidas = {
+                0,
+                1,
+                0,
+                1,
+                1,
+                -1,
+                2,
+                9,
+                4,
+                4,
+                10
+        };
+
+        for (int i = 0; i < entradas.length; i++) {
+            double resultado = primeiroCaractereUnico(entradas[i]);
+            double esperado = saidas[i];
+
+            System.out.println("Resultado: " + resultado);
+            System.out.println("Esperado: " + esperado);
+            System.out.println(resultado == esperado);
+            System.out.println();
+        }
+    }
+
+    public static int primeiroCaractereUnico(String input) {
+        int idxPrimeiroCharUnico = -1;
+        boolean achouIgual;
+        for(int i = 0; (i < input.length()) && (idxPrimeiroCharUnico == -1); i++) {
+
+            achouIgual = false;
+            for(int j = 0; (j < input.length()) && (!achouIgual); j++) {
+                if((i != j) && (input.charAt(i) == input.charAt(j))) {
+                    achouIgual = true;
+                }
+            }
+
+            if(!achouIgual) {
+                idxPrimeiroCharUnico = i;
+            }
+        }
+
+        return idxPrimeiroCharUnico;
+    }
+
+}
