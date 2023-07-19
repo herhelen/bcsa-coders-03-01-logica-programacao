d56ab6b27a40b8b79d540e044c10f1926a2e5966
Helen Her <her.helen@gmail.com>
2023-07-19 20:28:45 -0300

    adicionando resolução para exercício 3 da aula 4

---
+++ ada-logica-programacao/src/aula04/EX0403.java
@@ -0,0 +1,64 @@
+package aula04;
+
+public class EX0403 {
+
+    public static void main(String[] args) {
+        double[][] entradas = {
+                {8.0, 7.0, 8.0, 8.0},
+                {5.0, 4.3, 8.0, 7.0},
+                {2.0, 5.0, 8.0, 6.0},
+                {4.0, 1.0, 9.0, 7.0},
+                {2.0, 2.0, 10.0, 1.0},
+                {2.9, 2.5, 10.0, 7.1},
+                {4.4, 4.1, 8.5, 9.5},
+                {5.0, 7.0, 9.0, 8.0},
+                {8.0, 7.0, 8.0, 9.0},
+                {8.0, 8.0, 10.0, 9.0},
+                {2.0, 2.0, 5.0, 1.0}
+        };
+
+        String[] saidas = {
+                "B",
+                "C",
+                "C",
+                "D",
+                "D",
+                "C",
+                "C",
+                "B",
+                "B",
+                "A",
+                "D"
+        };
+
+        for (int i = 0; i < entradas.length; i++) {
+            String resultado = mediaAproveitamento(entradas[i]);
+            String esperado = saidas[i];
+
+            System.out.println("Resultado: " + resultado);
+            System.out.println("Esperado: " + esperado);
+            System.out.println(resultado.equals(esperado));
+            System.out.println();
+        }
+    }
+
+    public static String retornaConceito(double media) {
+        if(media >= 9.0) {
+            return "A";
+        } else if(media >= 7.5 && media < 9.0) {
+            return "B";
+        } else if(media >= 6.0 && media < 7.5) {
+            return "C";
+        }
+
+        // media < 6.0
+        return "D";
+    }
+
+    public static String mediaAproveitamento(double[] input) {
+        double media = input[0] + (input[1] * 2) + (input[2] * 3) + input[3];
+        media = media / 7;
+
+        return retornaConceito(media);
+    }
+}
