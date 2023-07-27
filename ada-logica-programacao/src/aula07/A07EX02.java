a515c363c48de236aadff4eeb4634aca13a78184
Helen Her <her.helen@gmail.com>
2023-07-26 21:15:18 -0300

    exerc´cio 2 aula 7 - na verdade é cálculo do fatorial. adicionando implementação do cálculo do fatorial usando for

--- ada-logica-programacao/src/aula07/A07EX02.java
+++ ada-logica-programacao/src/aula07/A07EX02.java
@@ -3,14 +3,24 @@ package aula07;
 public class A07EX02 {
 
     public static void main(String[] args) {
-        System.out.println(multiplicarRecursivo(5));
+        System.out.println(fatorial(0) == fatorialRecursivo(0));
+        System.out.println(fatorial(1) == fatorialRecursivo(1));
+        System.out.println(fatorial(5) == fatorialRecursivo(5));
     }
 
-    public static int multiplicarRecursivo(int limite) {
-        if(limite == 1) {
+    public static int fatorial(int numero) {
+        int resultado = 1;
+        for(int i = 1; i <= numero; i++) {
+            resultado *= i;
+        }
+        return resultado;
+    }
+
+    public static int fatorialRecursivo(int numero) {
+        if(numero == 0 || numero == 1) {
             return 1;
         }
 
-        return limite * multiplicarRecursivo(limite - 1);
+        return numero * fatorialRecursivo(numero - 1);
     }
 }
